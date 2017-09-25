77d74799a26d571631dd399e98df83cc7f0f5453
rybufc <rybufc@gmail.com>
2017-09-25 18:05:01 +0500

    Первая лаба, сделаны 2 алгоритма сортировки и намётки интерфейса проги

---
+++ Lab1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        private static List<Sort> sortMethods = new List<Sort>();
+        private static bool isEnded;
+        private static int[] sequence;
+        private static int iterations = 1;
+
+        public delegate int[] Sort(int[] toSort);
+
+        static void Main(string[] args)
+        {
+            sortMethods.Add(Sorting_Methods.BubbleSort);
+            sortMethods.Add(Sorting_Methods.ShellSort);
+
+            while (!isEnded)
+            {
+                getCommand(Console.ReadLine());
+            }
+        }
+
+        static void getCommand(string token)
+        {
+            string command = token.Split()[0];
+
+            switch (command)
+            {
+                case "help":
+                    Help();
+                    break;
+
+                case "iterations":
+                    iterations = int.Parse(token.Split()[1]);
+                    break;
+
+                case "sequence":
+                    sequence = GetSequence(GetTokens(token));
+                    break;
+
+                case "test":
+                    Test();
+                    break;
+
+                case "exit":
+                    isEnded = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Данной команды нет в списке доступных команд."+
+                    " Просьба за доступными командами обратиться в хелп по командам с помощью команды help.");
+                    break;
+            }
+        }
+
+        static string[] GetTokens(string token)
+        {
+            string[] parsedToken = token.Split();
+            string[] result = new string[parsedToken.Length - 1];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = parsedToken[i + 1];
+            }
+
+            return result;
+        }
+
+        static int[] GetSequence(string[] tokens)
+        {
+            int[] newSequence = new int[tokens.Length];
+            for (int i = 0; i < newSequence.Length; i++)
+            {
+                newSequence[i] = int.Parse(tokens[i]);
+            }
+            return newSequence;
+        }
+
+        static void Test()
+        {
+            Stopwatch timer = new Stopwatch();
+
+            foreach (var sort in sortMethods)
+            {
+                timer.Restart();
+                for (int i = 0; i < iterations; i++)
+                {
+                    sort(sequence);
+                }
+                timer.Stop();
+                Console.WriteLine(timer.Elapsed);
+            }
+        }
+
+        static void Help()
+        {
+            Console.WriteLine("Programm Help");
+        }
+    }
+}
---
+++ Lab1/Sorting Methods.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public static class Sorting_Methods
+    {
+        //Пузырёк
+        public static int[] BubbleSort(int[] toSort)
+        {
+            bool isSorted = false;
+            while (!isSorted)
+            {
+                isSorted = true;
+                for (int i = 0; i < toSort.Length - 1; i++)
+                {
+                    if (toSort[i] > toSort[i + 1])
+                    {
+                        int temp = toSort[i];
+                        toSort[i] = toSort[i + 1];
+                        toSort[i + 1] = temp;
+
+                        isSorted = false;
+                    }
+                }
+            }
+
+            return toSort;
+        }
+
+        //Сортировка Шелла
+        public static int[] ShellSort(int[] toSort)
+        {
+            for (int step = toSort.Length; step > 0; step /= 2)
+            {
+                for (int i = 0; i < step; i++)
+                {
+                    toSort = insertSort(toSort, step, i);
+                }
+            }
+
+            return toSort;
+        }
+
+        //сортировка вставкой
+        private static int[] insertSort(int[] toSort, int step, int i)
+        {
+            for (; i + step < toSort.Length; i += step)
+            {
+                if (toSort[i + step] < toSort[i])
+                {
+                    int temp = toSort[i];
+                    toSort[i] = toSort[i + 1];
+                    toSort[i + 1] = temp;
+
+                    for (int j = i; j - step >= 0 && toSort[j] < toSort[j - step]; j -= step)
+                    {
+                        temp = toSort[i];
+                        toSort[i] = toSort[i + 1];
+                        toSort[i + 1] = temp;
+                    }
+                }
+            }
+
+            return toSort;
+        }
+    }
+}
