2d0c7ffe5ea1d65f02696c26fe75ba670dc16615
rybufc <rybufc@gmail.com>
2017-10-27 18:15:10 +0500

    Added .ToString() and .TryParse() methods, other changes

--- Lab2/Rational.cs
+++ Lab2/Rational.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Lab2
 {
-    class Rational
+    public struct Rational
     {
         public int Numerator { get; set; }
         public int Denominator { get; set; }
@@ -14,7 +18,7 @@ namespace Lab2
         {
             get
             {
-                return this.Numerator / this.Denominator;
+                return Numerator / Denominator;
             }
         }
 
@@ -22,54 +26,92 @@ namespace Lab2
         {
             get
             {
-                return this.Numerator % this.Denominator;
+                return Numerator % Denominator;
             }
         }
 
+        public Rational(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+            Even();
+        }
+
+        
         public Rational Add(Rational c)
         {
-            Rational result = new Rational();
-            result.Denominator = LeastCommonMultiple(this.Denominator, c.Denominator);
-            result.Numerator = this.Numerator * result.Denominator / this.Denominator + c.Numerator * result.Denominator / c.Denominator;
-            result.Even();
-            return result;
+            return this + c;
         }
 
         public Rational Negate()
         {
-            Rational result = new Rational();
-            result.Numerator = 0 - this.Numerator;
-            result.Denominator = this.Denominator;
-            return result;
+            return 0 - this;
         }
 
-        public Rational Multiply(Rational x)
+        public Rational Multiply(Rational r)
         {
-            Rational result = new Rational();
-            result.Numerator = this.Numerator * x.Numerator;
-            result.Denominator = this.Denominator * x.Denominator;
-            result.Even();
-            return result;
+            return this + r;
         }
 
-        public Rational DivideBy(Rational x)
+        public Rational DivideBy(Rational r)
         {
-            Rational result = new Rational();
-            result.Numerator = this.Numerator * x.Denominator;
-            result.Denominator = this.Denominator * x.Numerator;
-            result.Even();
-            return result;
+            return this / r;
         }
 
         public override string ToString()
         {
-            
-            return base.ToString();
+            Even();
+            int z = Numerator / Denominator;
+            int numerator = Numerator - z * Denominator;
+
+            if (numerator == 0)
+            {
+                return string.Format("{}", z);
+            }
+            if (z == 0)
+            {
+                return string.Format("{}:{}", numerator, Denominator);
+            }
+
+            return string.Format("{}.{}:{}", z, numerator, Denominator
+            );
         }
         
         public static bool TryParse(string input, out Rational result)
-        {
+        {//Z.N:D,
+            if (input == "")
+            {
+                result = default(Rational);
+                return false;
+            }
 
+            Regex regexp = new Regex("^((\\d+)[.])?((\\d+)[:](\\d+))?$");
+            if (!regexp.IsMatch(input))
+            {
+                result = default(Rational);
+                return false;
+            }
+
+            string[] tokens = input.Split('.');
+            if (tokens.Length == 2)
+            {
+                int z = int.Parse(tokens[0]);
+                int numerator = int.Parse(tokens[1].Split(':')[0]);
+                int denominator = int.Parse(tokens[1].Split(':')[1]);
+                result = new Rational(numerator + z * denominator, denominator);
+                return true;
+            }
+
+            if (tokens[0].Split(':').Length == 2)
+            {
+                int numerator = int.Parse(tokens[0].Split(':')[0]);
+                int denominator = int.Parse(tokens[0].Split(':')[1]);
+                result = new Rational(numerator, denominator);
+                return true;
+            }
+
+            result = new Rational(int.Parse(tokens[0]), 1);
+            return true;
         }
 
         private void Even()
@@ -101,6 +143,76 @@ namespace Lab2
                 first = temp;
             }
             return first;
+        }
+
+        public static Rational operator +(Rational r1, Rational r2)
+        {
+            int denominator = LeastCommonMultiple(r1.Denominator, r2.Denominator);
+            int numerator = r1.Numerator * denominator / r1.Denominator + r2.Numerator * denominator / r2.Denominator;
+            Rational result = new Rational(numerator, denominator);
+            result.Even();
+            return result;
+        }
+
+        public static Rational operator +(Rational r1, int num)
+        {
+            return new Rational(r1.Numerator + num * r1.Denominator, r1.Denominator);
+        }
+
+        public static Rational operator +(int num, Rational r1)
+        {
+            return new Rational(r1.Numerator + num * r1.Denominator, r1.Denominator);
+        }
+
+        public static Rational operator -(Rational r1, Rational r2)
+        {
+            int denominator = LeastCommonMultiple(r1.Denominator, r2.Denominator);
+            int numerator = r1.Numerator * denominator / r1.Denominator - r2.Numerator * denominator / r2.Denominator;
+            return new Rational(numerator, denominator);
+        }
+
+        public static Rational operator -(Rational r1, int num)
+        {
+            return new Rational(r1.Numerator - num * r1.Denominator, r1.Denominator);
+        }
+
+        public static Rational operator -(int num, Rational r1)
+        {
+            return new Rational(num * r1.Denominator - r1.Numerator, r1.Denominator);
+        }
+
+        public static Rational operator *(Rational r1, Rational r2)
+        {
+            int denominator = r1.Denominator * r2.Denominator;
+            int numerator = r1.Numerator * r2.Numerator;
+            return new Rational(numerator, denominator);
+        }
+
+        public static Rational operator *(Rational r1, int num)
+        {
+            return new Rational(r1.Numerator * num, r1.Denominator);
+        }
+
+        public static Rational operator *(int num, Rational r1)
+        {
+            return new Rational(r1.Numerator * num, r1.Denominator);
+        }
+
+        public static Rational operator /(Rational r1, Rational r2)
+        {
+            int denominator = r1.Denominator * r2.Numerator;
+            int numerator = r1.Numerator * r2.Denominator;
+            return new Rational(numerator, denominator);
+        }
+
+        public static Rational operator /(Rational r1, int num)
+        {
+            return new Rational(r1.Numerator, r1.Denominator * num);
+        }
+
+        public static Rational operator /(int num, Rational r1)
+        {
+            return new Rational(r1.Denominator * num, r1.Numerator);
         }
     }
 }
