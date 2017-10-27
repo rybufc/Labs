3dbccfbb701022ad96d1dd0062cc8f01bc1e28e7
rybufc <rybufc@gmail.com>
2017-10-27 19:52:15 +0500

    Removed unnecessarry usings

--- Lab2/Rational.cs
+++ Lab2/Rational.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Lab2
 {
