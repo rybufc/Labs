47e4ec7006973fed20e01291fd25a684e40001d0
Patapata3 <32408943+Patapata3@users.noreply.github.com>
2017-11-23 00:40:04 +0500

    Update Picture.cs

--- GraphicsEditor/GraphicsEditor/Picture.cs
+++ GraphicsEditor/GraphicsEditor/Picture.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using DrawablesUI;
 
@@ -6,12 +6,12 @@ namespace GraphicsEditor
 {
     public class Picture : IDrawable
     {
-        private readonly List<IDrawable> shapes = new List<IDrawable>();
+        private readonly List<IShape> shapes = new List<IShape>();
         private readonly object lockObject = new object();
 
         public event Action Changed;
 
-        public void Remove(IDrawable shape)
+        public void Remove(IShape shape)
         {
             lock (lockObject)
             {
@@ -29,7 +29,7 @@ namespace GraphicsEditor
             }
         }
 
-        public void Add(IDrawable shape)
+        public void Add(IShape shape)
         {
             lock (lockObject)
             {
@@ -39,7 +39,7 @@ namespace GraphicsEditor
             }
         }
 
-        public void Add(int index, IDrawable shape)
+        public void Add(int index, IShape shape)
         {
             lock (lockObject)
             {
@@ -57,6 +57,28 @@ namespace GraphicsEditor
                 {
                     shape.Draw(drawer);
                 }
+            }
+        }
+
+        public void Information()
+        {
+            lock (lockObject)
+            {
+                int index = 0;
+                foreach (var shape in shapes)
+                {
+                    Console.Write("[{0}] ", index);
+                    Console.WriteLine(shape.ToString());
+                    index++;
+                }
+            }
+        }
+
+        public IShape GetShape(int index)
+        {
+            lock (lockObject)
+            {
+                return shapes[index];
             }
         }
     }
