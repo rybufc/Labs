48285154a77b1e3035a5e9a81ad44247bc0cc0b7
Patapata3 <32408943+Patapata3@users.noreply.github.com>
2017-11-23 00:40:34 +0500

    Update Point.cs

--- GraphicsEditor/GraphicsEditor/Point.cs
+++ GraphicsEditor/GraphicsEditor/Point.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using DrawablesUI;
 
 namespace GraphicsEditor
 {
-    class Point : IDrawable
+    class Point : IShape
     {
         public PointF Coordinates
         {
@@ -13,14 +13,26 @@ namespace GraphicsEditor
             private set;
         }
 
+        public FormatInfo Format { get; set; } = new FormatInfo();
+
         public Point(float x, float y)
         {
             Coordinates = new PointF(x, y);
+            Format.Color = Color.Black;
+            Format.Width = 1;
         }
 
-        public void Draw(IDrawer Drawer)
+        public void Draw(IDrawer drawer)
         {
-            Drawer.DrawPoint(Coordinates);
+            drawer.SelectPen(Format.Color, (int)Format.Width);
+            drawer.DrawPoint(Coordinates);
+        }
+
+        public override string ToString()
+        {
+
+            string result = string.Format("Точка({0}, {1})", Coordinates.X, Coordinates.Y);
+            return result;
         }
     }
 }
