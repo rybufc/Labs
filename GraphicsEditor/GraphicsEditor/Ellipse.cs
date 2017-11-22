80fd9f5b527600a26f40aa53f7de3af838a03f2a
Patapata3 <32408943+Patapata3@users.noreply.github.com>
2017-11-23 00:38:39 +0500

    Update Ellipse.cs

--- GraphicsEditor/GraphicsEditor/Ellipse.cs
+++ GraphicsEditor/GraphicsEditor/Ellipse.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ using DrawablesUI;
 
 namespace GraphicsEditor
 {
-    class Ellipse : IDrawable
+    class Ellipse : IShape
     {
         public PointF Centre
         {
@@ -28,16 +28,28 @@ namespace GraphicsEditor
             private set;
         }
 
+        public FormatInfo Format { get; set; } = new FormatInfo();
+
         public Ellipse(PointF centre, SizeF axises, float rotate)
         {
             Centre = centre;
             Axises = axises;
             Rotate = rotate;
+            Format.Color = Color.Black;
+            Format.Width = 1;
         }
 
-        public void Draw(IDrawer Drawer)
+        public void Draw(IDrawer drawer)
         {
-            Drawer.DrawEllipseArc(Centre, Axises, 0, 360, Rotate);
+            drawer.SelectPen(Format.Color, (int)Format.Width);
+            drawer.DrawEllipseArc(Centre, Axises, 0, 360, Rotate);
+        }
+
+        public override string ToString()
+        {
+            string result = string.Format("Эллипс(Центр({0}, {1}), Оси({2}, {3}), Угол = {4})",
+                Centre.X, Centre.Y, Axises.Height, Axises.Width, Rotate);
+            return result;
         }
     }
 }
